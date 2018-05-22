8c8815ca0d2558ae30d03af5978c2feae23477e1
Joffrey Bourdieux <scyl6x@gmail.com>
2018-05-22 22:35:15 +0200

    Change smallcoin to apsiocoin

--- src/irc.cpp
+++ src/irc.cpp
@@ -294,14 +294,14 @@ void ThreadIRCSeed2(void* parg)
         }
         
         if (fTestNet) {
-            Send(hSocket, "JOIN #smallchangeTEST3\r");
-            Send(hSocket, "WHO #smallchangeTEST3\r");
+            Send(hSocket, "JOIN #apsiocoinTEST3\r");
+            Send(hSocket, "WHO #apsiocoinTEST3\r");
         } else {
-            // randomly join #smallchange00-#smallchange99
+            // randomly join #apsiocoin00-#apsiocoin99
             int channel_number = GetRandInt(100);
             channel_number = 0; // Litecoin: for now, just use one channel
-            Send(hSocket, strprintf("JOIN #smallchange%02d\r", channel_number).c_str());
-            Send(hSocket, strprintf("WHO #smallchange%02d\r", channel_number).c_str());
+            Send(hSocket, strprintf("JOIN #apsiocoin%02d\r", channel_number).c_str());
+            Send(hSocket, strprintf("WHO #apsiocoin%02d\r", channel_number).c_str());
         }
 
         int64 nStart = GetTime();
